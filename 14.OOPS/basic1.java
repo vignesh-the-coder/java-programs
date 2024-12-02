0eab258e41c01b9ebfb9bfd76b47195d48d85534
vignesh.N <vnarisineni@gmail.com>
2024-12-02 19:19:05 +0530

    first commit

---
+++ 14.OOPS/basic1.java
@@ -0,0 +1,20 @@
+public class basic1{
+    public static void main(String args[]){
+       Pen p1 = new Pen();
+       p1.setColor("Blue");
+       System.out.println(p1.color);
+
+    }
+}
+
+class  Pen{
+    String color;
+    int tip;
+    void setColor(String newColor){
+          color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
