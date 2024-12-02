40a3561213937b14d6fd7c5e4a4af78cf335a70e
vignesh.N <vnarisineni@gmail.com>
2024-12-02 19:49:44 +0530

    change it

--- 14.OOPS/basic1.java
+++ 14.OOPS/basic1.java
@@ -4,6 +4,13 @@ public class basic1{
        p1.setColor("Blue");
        System.out.println(p1.color);
 
+       p1.setTip(5);
+       System.out.println(p1.tip);
+
+       p1.color = "Yellow";
+       System.out.println(p1.color);
+
+
     }
 }
 
